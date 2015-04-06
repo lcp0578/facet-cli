96e2505fcd403a480e56d028289934871c88ce50
Vadim Ogievetsky <vadimon@gmail.com>
2015-04-05 21:01:51 -0700

    basic working CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,53 @@
 /// <reference path="../typings/node/node.d.ts" />
 /// <reference path="../typings/nopt/nopt.d.ts" />
+/// <reference path="../node_modules/facetjs/build/facet.d.ts" />
 "use strict";
 
-interface FlagTypeMap {
-  [k: string]: Object
-}
-
-interface ShortFlags {
-  [k: string]: string[]|string
-}
-
 import nopt = require("nopt");
+import facet = require("facetjs");
 
-var knownOpts: FlagTypeMap = {
-  "flag" : Boolean,
-  "many" : [String, Array]
-};
+var Expression = facet.core.Expression;
 
-var shortHands: ShortFlags = {
-  "f" : ["--flag"]
-};
+export function usage() {
+  console.log(`
+Usage: facet [options]
 
-var parsed = nopt(knownOpts, shortHands, process.argv, 2);
-console.log(parsed);
+      --help        print this help message
+  -v, --version      display the version number
+  -h, --host        the host to connect to
+  -s, --sql         run this SQL query
+      --simulate    simulate this query
+  `)
+}
+
+export function version() {
+  console.log(`facet version 0.9.12 (cli version 0.1.0)`);
+}
+
+export function run() {
+  var parsed = nopt(
+    {
+      "host": String,
+      "help": Boolean,
+      "sql": String,
+      "simulate": Boolean,
+      "version": Boolean
+    },
+    {
+      "h": ["--host"],
+      "s": ["--sql"],
+      "v": ["--version"]
+    },
+    process.argv,
+    2
+  );
+
+  if (parsed.argv.original.length === 0 || parsed['help']) return usage();
+
+  if (parsed['version']) return version();
+
+  var sql: string = parsed['sql'];
+  if (sql) {
+    console.log(Expression.parseSQL(sql).toJS());
+  }
+}
